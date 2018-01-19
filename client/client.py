67ce31915ffb78e79618abae8d183987ebd1e041
Mijura <miodragvilotijevic@gmail.com>
2018-01-19 14:01:19 +0100

    izmenjeno osluskivanje promena, dodat tcp handler

--- client/client.py
+++ client/client.py
@@ -1,9 +1,9 @@
 import pygame
 from threading import Thread
-from listener import *
-
-def square(x):
-    return x**2
+from traffic import *
+import socketserver
+from contextlib import closing
+import socket
 
 class Client:
     
@@ -17,23 +17,32 @@ class Client:
         self.display.blit(self.bg, (0, 0))
         pygame.display.flip()
         
-        #instancing object for traffic
-        self.traffic = Traffic(self)
+        self.player_coord = {'1': (5, 345), '2': (5, 105), '3': (325, 30), '4': (645, 105), '5': (645, 345), '6': (325, 420)}
+        self.empty_coord = {'1': (55, 390), '2': (55, 105), '3': (355, 45), '4': (645, 105), '5': (645, 390), '6': (355, 450)}
 
-        #registering player on server (sending name, address, port)
-        self.traffic.register_player()
+        self.HOST, self.PORT = self.get_address()
+        self.address = ''.join([self.HOST,':', str(self.PORT)])
+        
+        #instancing object for sender
+        self.sender = Sender(self)
+
+        #registering player on server
+        self.sender.register_player(self.address, "mio")
 
         #get players in the game
-        self.traffic.get_players()
+        self.sender.get_players()
 
         #wait for changes from core server
         #must be on thread because method for listening use infinity loop
-        listen = Thread(target = self.traffic.listen, args = ())
+        listen = Thread(target = self.listen, args = ())
         listen.start()
+
+        t_s = Thread(target = self.sender.take_seat, args = (self.address, "1"))
+        t_s.start()
 
         self.game_loop()
         listen.join()
- 
+
     def blit_alpha(self, target, source, location, opacity):
         x = location[0]
         y = location[1]
@@ -43,40 +52,76 @@ class Client:
         temp.set_alpha(opacity)        
         target.blit(temp, location)
     
-    def take_seat(func):
+    def draw_player(func):
         def callf(self, news):
             if(news['message']=='take seat'):
-                self.display.blit(pygame.image.load("images/purple_right.png"), (5, 105))
+                seat = news['seat']
+                previous = pygame.image.load("images/take.png")
+                if(int(seat)>=4):
+                    side = 'left'
+                else:
+                    side = 'right'
+                self.display.blit(self.bg, self.empty_coord[seat], pygame.Rect(self.empty_coord[seat], previous.get_rect().size))
+                self.display.blit(pygame.image.load("images/purple_"+side+".png"), self.player_coord[seat])
             return func(self, news)
         return callf
 
     def move(func):
         def callf(self, news):
             if(news['message']=='move'):
-                self.display.blit(pygame.image.load("images/purple_right.png"), (5, 105))
+                seat = news['seat']
+                self.display.blit(pygame.image.load("images/purple_right.png"), self.player_coord[seat])
             return func(self, news)
         return callf
 
-    def convert_players_to_news(self, player):
-        player['message'] = 'take seat'
-        return player
+    def take_button(func):
+        def callf(self, news):
+            if(news['message']=='take button'):
+                seat = news['seat']
+                self.display.blit(pygame.image.load("images/take.png"), self.empty_coord[seat])
+            return func(self, news)
+        return callf
+
+    def init_table(self, seat):
+        if(str(seat) in self.players):
+            player = self.players[str(seat)]
+            player['message'] = 'take seat'
+            result = player
+        else:
+            result = {'message':'take button', 'seat': str(seat)}
+        return result
 
     @move
-    @take_seat
+    @draw_player
+    @take_button
     def refresh_table(self, news):
         pygame.display.flip()
 
-    def draw_players(self, players):
-        news = map(self.convert_players_to_news, list(players.values()))
+    def draw_seats(self, players):
+        self.players = players
+        news = map(self.init_table, range(1,7))
         for n in list(news):
             self.refresh_table(n)
+
+    def get_address(self):
+        HOST = socket.gethostbyname(socket.gethostname()) # get ip address
+        with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
+            s.bind((HOST, 0))
+            PORT = s.getsockname()[1]
+            return HOST, PORT
+
+    def listen(self):
+        # Create the server, binding to localhost and port
+        with socketserver.TCPServer((self.HOST, self.PORT), MyTCPHandler(self)) as server:
+            # Activate the server; this will keep running until you
+            # interrupt the program with Ctrl-C
+            server.serve_forever()
         
     def game_loop(self):
         gameExit = False
         while not gameExit:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    self.traffic.done = True
                     gameExit = True
 
         pygame.quit()
@@ -84,4 +129,3 @@ class Client:
         
 if __name__ == "__main__":
     c = Client()
-    
---
+++ client/traffic.py
@@ -0,0 +1,59 @@
+'''
+    Class for communication with remote server (core)
+'''
+import socketserver
+import json
+import requests
+
+class Sender():
+    
+    def __init__(self, client):
+    
+        self.client = client
+        self.remote_server = "http://localhost:8080"
+    
+    def register_player(self, address, name):
+        r = requests.post(self.remote_server + "/registerPlayer", 
+                            data={'name': name, 'address': address})
+
+    def get_players(self):
+        r = requests.get(self.remote_server + "/inGame")
+        data = json.loads(r.text)
+        self.client.draw_seats(data)
+
+    def take_seat(self, address, number):
+        r = requests.post(self.remote_server + "/takeSeat", 
+                            data={'seat': number, 'address': address})
+        result = json.loads(r.text)
+        if(not result):
+            print("seat is busy")
+
+class MyTCPHandler(socketserver.BaseRequestHandler):
+    """
+    The request handler class for our server.
+
+    It is instantiated once per connection to the server, and must
+    override the handle() method to implement communication to the
+    client.
+    """
+
+    def __init__(self, client):
+        self.client = client
+
+    def __call__(self, request, client_address, server):
+        h = MyTCPHandler(self.client)
+        socketserver.BaseRequestHandler.__init__(h, request, client_address, server)
+
+    def handle(self):
+        # self.request is the TCP socket connected to the client
+        self.data = self.request.recv(1024).strip()
+        print("{} wrote:".format(self.client_address[0]))
+        print(self.data)
+        
+        data = self.data.decode('UTF-8').split('\r\n\r\n')[1] #split request with new line (header is at position 0, data is at position 1)
+        data = json.loads(data)
+        self.client.refresh_table(data)
+
+        # just send back the same data, but upper-cased
+        response = b'HTTP/1.1 200 OK\n\n'
+        self.request.sendall(response)
