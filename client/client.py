8adb1d170c0a0cddb1f0450dd0f88e4b70cbfd17
Mijura <miodragvilotijevic@gmail.com>
2018-02-21 17:45:45 +0100

    implementirane klase Player i EmptySeat

--- client/client.py
+++ client/client.py
@@ -9,47 +9,6 @@ from widgets import *
 import functools
 import abc
 
-class Slider():
-    def __init__(self):
-        self.WHITE = (255, 255, 255)
-        self.TRANS = (1, 1, 1)
-
-        self.val = 0  # start value
-        self.maxi = 0  # maximum at slider position right
-        self.mini = 0  # minimum at slider position left
-        self.xpos = 540  # x-location on screen
-        self.ypos = 495 # y-location on screen
-
-        self.hit = False  # the hit attribute indicates slider movement due to mouse interaction
-
-        # button surface #
-        self.button_surf = pygame.surface.Surface((20, 20))
-        self.button_surf.fill(self.TRANS)
-        self.button_surf.set_colorkey(self.TRANS)
-        self.button_surf.blit(pygame.image.load("images/slider_button.png"),(0,0))
-
-    def draw(self, screen):
-        screen.blit(pygame.image.load("images/table.png"), (self.xpos, self.ypos), pygame.Rect((self.xpos, self.ypos),(120, 25)))
-        surf = pygame.surface.Surface((120, 25), pygame.SRCALPHA, 32)
-        surf.blit(pygame.image.load("images/slider_scale.png"),(0,8))
-
-        pos = (10+int((self.val-self.mini)/(self.maxi-self.mini)*100), 13)
-        self.button_rect = self.button_surf.get_rect(center=pos)
-        surf.blit(self.button_surf, self.button_rect)
-        self.button_rect.move_ip(self.xpos, self.ypos)  # move of button box to correct screen position
-
-        screen.blit(surf, (self.xpos, self.ypos))
-    
-    def move(self):
-        """
-        The dynamic part; reacts to movement of the slider button.
-        """
-        self.val = (pygame.mouse.get_pos()[0] - self.xpos - 10) / 100 * (self.maxi - self.mini) + self.mini
-        if self.val < self.mini:
-            self.val = self.mini
-        if self.val > self.maxi:
-            self.val = self.maxi
-
 class Client:
     
     def __init__(self, name):
@@ -59,6 +18,7 @@ class Client:
         self.name = name
         
         self.buttons = []
+        self.table = {}
         self.thread_lock = Lock()
         self.last_clicked_button = None
         self.button_args = None
@@ -73,11 +33,11 @@ class Client:
 
         pygame.display.flip()
         
-        self.player_coord = {'1': (5, 345), '2': (5, 105), '3': (325, 30), '4': (645, 105), '5': (645, 345), '6': (325, 420)}
-        self.empty_coord = {'1': (55, 390), '2': (55, 105), '3': (355, 45), '4': (645, 105), '5': (645, 390), '6': (355, 450)}
-        self.cards_coord = {'1': (5, 320), '2': (5, 80), '3': (325, 5), '4': (675, 80), '5': (675, 320), '6': (355, 395)}
+        self.player_coord = {1: (5, 345), 2: (5, 105), 3: (325, 30), 4: (645, 105), 5: (645, 345), 6: (325, 420)}
+        self.empty_coord = {1: (55, 390), 2: (55, 105), 3: (355, 45), 4: (645, 105), 5: (645, 390), 6: (355, 450)}
+        self.cards_coord = {1: (5, 320), 2: (5, 80), 3: (325, 5), 4: (675, 80), 5: (675, 320), 6: (355, 395)}
         self.buttons_coord = {'check': (410, 527), 'call': (410, 527), 'raise': (540, 527), 'bet': (540, 527), 'fold': (670, 527)}
-        self.chips_coord = {'1': (190, 325), '2': (190, 150), '3': (400, 110), '4': (590, 150), '5': (590, 325), '6': (400, 370)}
+        self.chips_coord = {1: (190, 325), 2: (190, 150), 3: (400, 110), 4: (590, 150), 5: (590, 325), 6: (400, 370)}
         self.chips = [1, 5, 10, 25, 50, 100, 200, 500, 1000]
         self.stake_keys = ['bet','raise','call','big blind', 'small blind']
 
@@ -99,16 +59,6 @@ class Client:
         self.listen_thread.start()
 
         self.game_loop()
-
-    #draws image with opacity
-    def blit_alpha(self, source, location, opacity):
-        x = location[0]
-        y = location[1]
-        temp = pygame.Surface((source.get_width(), source.get_height())).convert()
-        temp.blit(self.display, (-x, -y))
-        temp.blit(source, (0, 0))
-        temp.set_alpha(opacity)        
-        self.display.blit(temp, location)
     
     def remove_seat(func):
         def callf(self, news):
@@ -119,70 +69,12 @@ class Client:
                 self.display.blit(self.bg, p_coords, pygame.Rect(p_coords, previous.get_rect().size))
             return func(self, news)
         return callf
-
-    def draw_player(func):
-        def callf(self, news):
-            if('draw player' in news):
-                seat = news['seat']
-                previous = pygame.image.load("images/take.png")
-                p_coords = self.empty_coord[seat]
-
-                #delete button from dictionary and screen
-                """
-                self.thread_lock.acquire()
-                if p_coords in self.buttons:
-                    del self.buttons[p_coords]
-                self.thread_lock.release()
-                """
-
-                x, y = self.player_coord[seat]
-
-                name_label = self.myfont.render(news['name'], True, pygame.Color('white'))
-                l_size = name_label.get_rect().size
-                chips_label = self.myfont.render(str(news['chips'])+' $', True, pygame.Color('white'))
-                c_size = chips_label.get_rect().size
-
-                if(int(seat)>=4):
-                    side = 'left'
-                    l_x = x + 100 - l_size[0]/2
-                    c_x = x + 100 - c_size[0]/2    
-                else:
-                    side = 'right'
-                    l_x = x + 50 - l_size[0]/2
-                    c_x = x + 50 - c_size[0]/2
-
-                if('on move' in news):
-                    color = "green"
-                else:
-                    color = "purple"
-                
-                self.display.blit(pygame.image.load("images/"+color+"_"+side+".png"), (x, y))
-                
-                l_y = y + 15
-                c_y = y + 37
-                self.display.blit(name_label, (l_x, l_y))
-                self.display.blit(chips_label, (c_x, c_y))
-            return func(self, news)
-        return callf
-
+    
     def draw_image_part(self, image, coord, size):
         self.display.blit(image, coord, pygame.Rect((0,0), size))
 
     # creates button and save button args if user click on button (left click)
     def create_button(self, image, coord, action, action_args, type):
-        mouse = pygame.mouse.get_pos()
-        click = pygame.mouse.get_pressed()
-    
-        x, y = coord
-        w,h = image.get_rect().size
-        self.display.blit(self.bg, coord, pygame.Rect(coord, image.get_rect().size))
-        if x+w>mouse[0]>x and y+h>mouse[1]>y:
-            self.display.blit(image, (x, y))
-            if(click[0]==1):
-                self.button_args = (image, coord, action, action_args, type)
-        else:
-            self.blit_alpha(image, (x, y), 210)
-        
         if type=='raise' or type=='bet':
             myfont = pygame.font.Font("myriad_pro.ttf", 15)
             label = myfont.render(str(round(self.slider.val)), True, (255,255,255))
@@ -204,34 +96,6 @@ class Client:
                 else:
                     self.draw_image_part(pygame.image.load("images/cards/0.png"),(x, y), p)
                     self.draw_image_part(pygame.image.load("images/cards/0.png"),(x+60, y), p)
-            return func(self, news)
-        return callf
-    
-    # adds button in dictionary
-    def draw_take_button(func):
-        def callf(self, news):
-            if('draw take button' in news):
-                seat = news['seat']
-                key = self.empty_coord[seat]
-                value = (pygame.image.load("images/take.png"), 
-                         self.empty_coord[seat], 
-                         self.sender.take_seat, 
-                         (self.address, seat), 'seat')
-                self.thread_lock.acquire()
-                self.buttons.append(TakeSeatButton(self.empty_coord[seat], seat, self))
-                self.thread_lock.release()
-            return func(self, news)
-        return callf
-
-    def draw_empty_seat(func):
-        def callf(self, news):
-            if('draw empty seat' in news):
-                seat = news['seat']
-                coords = self.empty_coord[seat]
-                self.display.blit(self.bg, coords, 
-                    pygame.Rect(coords, pygame.image.load("images/take.png").get_rect().size))
-                        
-                self.blit_alpha(pygame.image.load("images/empty.png"), coords, 128)
             return func(self, news)
         return callf
 
@@ -363,42 +227,72 @@ class Client:
     def player_or_take(self, seat):
         if(str(seat) in self.players):
             player = self.players[str(seat)]
-            result = player
+            return player
         else:
-            result = {'draw take button':True, 'seat': str(seat)}
-        return result
+            return {'set take button':True, 'seat': str(seat)}
 
     def post_take(self, seat):
         if(str(seat) in self.players):
-            result = {}
+            player = self.players[str(seat)]
+            return {}
         else:
-            result = {'draw empty seat':True, 'seat': str(seat)}
-        return result
+            return {'set empty seat':True, 'seat': str(seat)}
+
+    def set_player(func):
+        def callf(self, news):
+            if('set player' in news):
+                seat = int(news['seat'])
+                self.thread_lock.acquire()
+                self.table[seat] = Player(self.player_coord[seat], seat, news['name'], news['chips'], 
+                        news['on move'], news['bet'], self)
+                self.thread_lock.release()
+            return func(self, news)
+        return callf
+
+    def set_empty_seat(func):
+        def callf(self, news):
+            if('set empty seat' in news):
+                seat = int(news['seat'])
+                self.thread_lock.acquire()
+                self.table[seat] = EmptySeat(self.empty_coord[seat], seat, self)
+                self.thread_lock.release()
+            return func(self, news)
+        return callf
+
+    def set_take_button(func):
+        def callf(self, news):
+            if('set take button' in news):
+                seat = int(news['seat'])
+                self.thread_lock.acquire()
+                self.table[seat] = TakeSeatButton(self.empty_coord[seat], seat, self)
+                self.thread_lock.release()
+            return func(self, news)
+        return callf
 
     #@draw_chips
-    #@remove_seat
     #@draw_bet_buttons
     #@draw_call_button
     #@draw_check_button
     #@draw_players_cards
-    @draw_player
-    @draw_take_button
-    @draw_empty_seat
+    @set_player
+    @set_take_button
+    @set_empty_seat
     def refresh_table(self, news):
         pygame.display.flip()
 
     def init_table(self, players):
         self.players = players
-        news = map(self.player_or_take, range(1,7))
-        for n in list(news):
-            self.refresh_table(n)
+        print(players)
+        seats = map(self.player_or_take, range(1,7))
+        for seat in seats:
+            self.refresh_table(seat)
 
     def draw_empty_seats(self, players):
         self.players = players
-        news = map(self.post_take, range(1,7))
-        for n in list(news):
-            self.refresh_table(n)
-
+        empty_seats = map(self.post_take, range(1,7))
+        for seat in empty_seats:
+            self.refresh_table(seat)
+        
     #return ip address and free port
     def get_address(self):
         HOST = socket.gethostbyname(socket.gethostname()) # get ip address
@@ -414,18 +308,18 @@ class Client:
             # interrupt the program with Ctrl-C
             self.server.serve_forever()
     
-    #Update the display and show the button
-    def show_the_buttons(self):
+    #Update the display
+    def update_table(self):
         self.thread_lock.acquire()
-        for button in self.buttons:
-            button.draw()
+        for widget in self.table.values():
+            widget.draw()
         self.thread_lock.release()
         pygame.display.flip()
 
     def game_loop(self):
         gameExit = False
         while not gameExit:
-            self.show_the_buttons() #each time, draws all buttons from dictionary
+            self.update_table() #each time, draws all seats
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.server.shutdown()
@@ -443,10 +337,11 @@ class Client:
                     t = Thread(target = self.last_clicked_button.mouse_click, args = {})
                     t.start()
                     
-                    #erase all buttons from screen
-                    for button in self.buttons:
-                        button.erase()
-                    pygame.display.flip()
+                    self.last_clicked_button.erase()
+
+                    self.thread_lock.acquire()
+                    del self.table[self.last_clicked_button.seat_number]
+                    self.thread_lock.release()
 
                     self.last_clicked_button = None
                     self.buttons = []
--- client/widgets.py
+++ client/widgets.py
@@ -64,3 +64,107 @@ class TakeSeatButton(Button):
 
     def mouse_click(self):
         self.parent.sender.take_seat(self.parent.address, self.seat_number)
+    
+class Slider(Widget):
+    def __init__(self):
+        self.WHITE = (255, 255, 255)
+        self.TRANS = (1, 1, 1)
+
+        self.val = 0  # start value
+        self.maxi = 0  # maximum at slider position right
+        self.mini = 0  # minimum at slider position left
+        self.xpos = 540  # x-location on screen
+        self.ypos = 495 # y-location on screen
+
+        self.hit = False  # the hit attribute indicates slider movement due to mouse interaction
+
+        # button surface #
+        self.button_surf = pygame.surface.Surface((20, 20))
+        self.button_surf.fill(self.TRANS)
+        self.button_surf.set_colorkey(self.TRANS)
+        self.button_surf.blit(pygame.image.load("images/slider_button.png"),(0,0))
+
+    def draw(self, screen):
+        screen.blit(pygame.image.load("images/table.png"), (self.xpos, self.ypos), pygame.Rect((self.xpos, self.ypos),(120, 25)))
+        surf = pygame.surface.Surface((120, 25), pygame.SRCALPHA, 32)
+        surf.blit(pygame.image.load("images/slider_scale.png"),(0,8))
+
+        pos = (10+int((self.val-self.mini)/(self.maxi-self.mini)*100), 13)
+        self.button_rect = self.button_surf.get_rect(center=pos)
+        surf.blit(self.button_surf, self.button_rect)
+        self.button_rect.move_ip(self.xpos, self.ypos)  # move of button box to correct screen position
+
+        screen.blit(surf, (self.xpos, self.ypos))
+    
+    def erase(self):
+        pass
+    
+    def move(self):
+        """
+        The dynamic part; reacts to movement of the slider button.
+        """
+        self.val = (pygame.mouse.get_pos()[0] - self.xpos - 10) / 100 * (self.maxi - self.mini) + self.mini
+        if self.val < self.mini:
+            self.val = self.mini
+        if self.val > self.maxi:
+            self.val = self.maxi
+
+class Player(Widget):
+
+    def __init__(self, position, seat_number, name, chips, on_move, bet, client):
+        self.position = position
+        self.seat_number = seat_number
+        self.name = name
+        self.chips = chips
+        self.on_move = on_move
+        self.bet = bet
+        self.parent = client
+
+    def draw(self):
+        x, y = self.position
+
+        name_label = self.parent.myfont.render(self.name, True, pygame.Color('white'))
+        l_size = name_label.get_rect().size
+        chips_label = self.parent.myfont.render(str(self.chips)+' $', True, pygame.Color('white'))
+        c_size = chips_label.get_rect().size
+
+        if(self.seat_number >= 4):
+            side = 'left'
+            l_x = x + 100 - l_size[0]/2
+            c_x = x + 100 - c_size[0]/2    
+        else:
+            side = 'right'
+            l_x = x + 50 - l_size[0]/2
+            c_x = x + 50 - c_size[0]/2
+
+        if(self.on_move):
+            color = "green"
+        else:
+            color = "purple"
+        
+        self.image = pygame.image.load("images/"+color+"_"+side+".png")
+        self.erase()
+        self.parent.display.blit(self.image, self.position)
+                
+        l_y = y + 15
+        c_y = y + 37
+        self.parent.display.blit(name_label, (l_x, l_y))
+        self.parent.display.blit(chips_label, (c_x, c_y))
+    
+    def erase(self):
+        self.parent.display.blit(self.parent.bg, self.position, pygame.Rect(self.position, self.image.get_rect().size))
+
+class EmptySeat(Widget):
+
+    def __init__(self, position, seat_number, client):
+        self.position = position
+        self.seat_number = seat_number
+        self.image = pygame.image.load("images/empty.png")
+        self.parent = client
+
+    def draw(self):
+        self.erase()
+        blit_alpha(self.parent.display, self.image, self.position, 128)
+                
+    def erase(self):
+        self.parent.display.blit(self.parent.bg, self.position, pygame.Rect(self.position, self.image.get_rect().size))
